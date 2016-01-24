7417159b150476d833ae46c5b7a15439e8462709
Rogelio Sevilla <rogelio.sevilla1@gmail.com>
2016-01-24 17:03:39 -0600

    first version for List enumerable class

--- lib/appilf/appilf_list.rb
+++ lib/appilf/appilf_list.rb
@@ -1,11 +1,25 @@
 module Appilf
 
-  class AppilfList
+  class AppilfList < AppilfObject
+    include Enumerable
 
-    attr_accessor :links
+    def elements
+      @elements ||= []
+    end
 
+    def initialize(api_elements_hash)
+      self.meta = api_element_hash.delete('meta').methodize!; rescue NoMethodError
+      self.links = api_element_hash.delete('links').methodize!; rescue NoMethodError
+      api_elements_hash['data'].each do |api_element_hash|
+        self.elements << Util.translate_from_response(api_element_hash)
+      end
+    end
 
-
+    def each
+      self.elements.each do |element|
+        yield element
+      end
+    end
 
   end
 
