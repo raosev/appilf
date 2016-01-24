c487c9bb6f7d5e740e172534d3c1f2cf0e7215a5
Rogelio Sevilla <rogelio.sevilla1@gmail.com>
2016-01-24 17:05:20 -0600

    update general requirement statements

--- lib/appilf.rb
+++ lib/appilf.rb
@@ -1,10 +1,17 @@
 require 'rest-client'
 require 'json'
+require 'methodize/hash'
 require 'appilf/version'
 require 'appilf/error'
 require 'appilf/api_actions'
 require 'appilf/authentication'
+require 'appilf/appilf_object'
+require 'appilf/appilf_list'
+require 'appilf/paginated_resource'
 require 'appilf/client'
+require 'appilf/listing'
+require 'appilf/user'
+require 'appilf/util'
 
 module Appilf
 
