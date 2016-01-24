1860541899ae8523a20864e72f08f0b283716341
Rogelio Sevilla <rogelio.sevilla1@gmail.com>
2016-01-24 17:00:39 -0600

    first version for Listing model

--- lib/appilf/listing.rb
+++ lib/appilf/listing.rb
@@ -1,7 +1,34 @@
 module Appilf
 
+  class Listing < AppilfObject
+    extend APIActions
+    extend PaginatedResource
 
-  class Listing
+    LISTING_PATH = "/listings"
+
+    def self.list
+      response = api_get(LISTING_PATH)
+      set_page(response)
+    end
+
+    def self.retrieve(listing_id)
+      response = api_get("#{LISTING_PATH}/#{listing_id}")
+      new(response)
+    end
+
+    def initialize(listing_hash)
+      add_relationships(listing_hash['data'].delete('relationships'))
+      super
+    end
+
+    def add_relationships(relationships_hash)
+      self.class.instance_eval do
+        relationships_hash.each_pair do |k, v|
+          define_method(k) { Util.translate_from_response(v) }
+        end
+      end
+    end
+
 
   end
 
