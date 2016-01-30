6775cf07eb108263f37d294fc819709978d0100c
Rogelio Sevilla <rogelio.sevilla1@gmail.com>
2016-01-30 06:45:33 -0600

    fix appilf list enumerable impl

--- lib/appilf/appilf_list.rb
+++ lib/appilf/appilf_list.rb
@@ -3,22 +3,20 @@ module Appilf
   class AppilfList < AppilfObject
     include Enumerable
 
-    def elements
-      @elements ||= []
+    attr_accessor :list_elements
+
+    def list_elements
+      @list_elements ||= []
     end
 
     def initialize(api_elements_hash)
-      self.meta = api_element_hash.delete('meta').methodize!; rescue NoMethodError
-      self.links = api_element_hash.delete('links').methodize!; rescue NoMethodError
       api_elements_hash['data'].each do |api_element_hash|
-        self.elements << Util.translate_from_response(api_element_hash)
+        self.list_elements << Util.translate_from_response({'data' => api_element_hash})
       end
     end
 
-    def each
-      self.elements.each do |element|
-        yield element
-      end
+    def each(&block)
+      self.list_elements.each(&block)
     end
 
   end
