d50a2be585f425f7f86b02f2d04160c5ffa2622c
Rogelio Sevilla <rogelio.sevilla1@gmail.com>
2016-02-06 13:07:46 -0600

    update appilf require statements

--- lib/appilf.rb
+++ lib/appilf.rb
@@ -1,17 +1,23 @@
+# third parties
 require 'rest-client'
 require 'json'
 require 'methodize/hash'
+
+# base classes
+require 'appilf/appilf_object'
+require 'appilf/appilf_list'
+
+# core
 require 'appilf/version'
 require 'appilf/error'
 require 'appilf/api_actions'
 require 'appilf/authentication'
-require 'appilf/appilf_object'
-require 'appilf/appilf_list'
 require 'appilf/paginated_resource'
 require 'appilf/client'
 require 'appilf/listing'
 require 'appilf/user'
 require 'appilf/util'
+
 
 module Appilf
 
